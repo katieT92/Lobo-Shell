56703c0315cf5f49254948e816fce2013aa4437c
Kyler <schnaible@protonmail.com>
2021-05-01 20:56:47 -0700

    started work on function to check commands
    added a possible solution to checking commands to execute the right execv(). Grabs the characters in line_words, and adds their ascii values to find commands value. escaped when it runs into a space.

--- include/parsetools.h
+++ include/parsetools.h
@@ -1,10 +1,21 @@
 #ifndef PARSETOOLS_H
 #define PARSETOOLS_H
 
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+
 // Parse a command line into a list of words,
 //    separated by whitespace.
 // Returns the number of words
 //
-int split_cmd_line(char* line, char** list_to_populate); 
+int split_cmd_line(char* line, char** list_to_populate);
+int pipeCount(char** line_words, int numberOfWords);
+int check_command(char* line_words);
 
 #endif
--- src/parsetools.c
+++ src/parsetools.c
@@ -9,7 +9,100 @@
 //    separated by whitespace.
 // Returns the number of words
 //
+int split_cmd_line(ch#include "constants.h"
+#include "parsetools.h"
+
+
+
+// Parse a command line into a list of words,
+//    separated by whitespace.
+// Returns the number of words
+//
 int split_cmd_line(char* line, char** list_to_populate) {
+   char* saveptr;  // for strtok_r; see http://linux.die.net/man/3/strtok_r
+   char* delimiters = " \t\n"; // whitespace
+   int i = 0;
+
+   list_to_populate[0] = strtok_r(line, delimiters, &saveptr);
+
+   while(list_to_populate[i] != NULL && i < MAX_LINE_WORDS - 1)  {
+       list_to_populate[++i] = strtok_r(NULL, delimiters, &saveptr);
+   };
+
+   return i;
+}
+
+//IN: Char ** , int
+//OUT: Integer
+//checks and counts number of | symbols  in the line_words array based on the number of words in that array.
+//If there are no pipes will return 0
+//If there are pipes, will return the number of pipes found.
+int pipeCount(char** line_words, int numberOfWords){
+    int pipeCount = 0;
+    for(int i = 0; i < numberOfWords; i++){
+        if(strchr(line_words[i], '|') != NULL){
+            if((strlen(line_words[i])) == 1){
+                pipeCount++;
+            }
+        }
+    }
+    return pipeCount;
+}
+
+int check_command(char* line_words){
+    int command = 0;
+    
+    for (int i=0; i < sizeof(line_words); i++) {
+        if (line_words[i] == 32 || line_words[i] == 45){       // If value at i is a space, '-', and possibly '|'
+            break;
+        }
+        command += line_words[i];
+    }
+
+    //printf("command value: %d\n", command);
+
+    switch (command) {
+        // Sometimes the first comman entered returns a different value than it does if entered after the first.
+        // Seems to work better if you use a comman that starts with w?
+        // Maybe some way to send a dummy value first to make it working
+        // Or just scrap this idea of checking ascii value of command 
+        case -1: 
+            break;
+        case  350: 
+            printf("EXECUTE ls COMMAND\n");
+
+            // https://support.sas.com/documentation/onlinedoc/sasc/doc/lr2/execv.htm
+            pid_t pid;
+            //char *const parmList[] = {"/bin/ls", "-l", "/u/userid/dirname", NULL};
+            char *const parmList[] = {"/bin/ls"};
+            if ((pid = fork()) == -1)
+                perror("fork error");
+            else if (pid == 0) {
+                execv("/bin/ls", parmList);
+                printf("Return not expected. Must be an execv error.n");
+            }
+            
+            break;
+        case  354: 
+            printf("EXECUTE ps COMMAND\n");
+            break;
+        case  461: 
+            printf("EXECUTE who COMMAND\n");
+            break;
+        case  345: 
+            printf("EXECUTE wc COMMAND\n");
+            break;
+        case  357: 
+            printf("EXECUTE tr COMMAND\n");
+            break;
+        case  439: 
+            printf("EXECUTE cat COMMAND\n");
+            break;
+        default:
+            break;
+    }
+}
+ar* line, char** list_to_populate) {
    char* saveptr;  // for strtok_r; see http://linux.die.net/man/3/strtok_r
    char* delimiters = " \t\n"; // whitespace
    int i = 0;
